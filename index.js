64e9896b24a0768f6ee1d37c621c483e17db5035
annarudi7 <annarudi776@gmail.com>
2026-09-05 22:44:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is running')).listen(process.env.PORT || 3000);
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
